796d800d06b9ad2400add97b3a38c89fe559c7b7
David Oduobuk <147693280+Oduobuk@users.noreply.github.com>
2025-01-20 04:21:16 +0100

    Update diabetes_prediction_with_app.py

--- diabetes_prediction_with_app.py
+++ diabetes_prediction_with_app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # loading the saved model
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('Diabetes-prediction-app/trained_model.sav', 'rb'))
 
 def diabetes_prediction(input_data):
     # Convert input data to numpy array
